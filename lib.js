2f102c7bea0d187e6404645d6cedc8fb2ca801b9
development <development@jonathansblog.co.uk>
2014-08-20 18:57:22 +0100

    initial mods to code

--- lib.js
+++ lib.js
@@ -1,48 +1,50 @@
-// regex - modified from http://regex101.com/r/fH3lV1/2 to fit sterling ( optional , as thousand separator . as decimal separator)
-
-// - explination at http://regex101.com/r/hT3aD0/1
-// ^\s*-?(\u00A3|\s*)((\d{1,3}(,(\d){3})*)|\d*)(\.\d{1,2})?(\s?|p)\s*$
-// regex doesnt make £p invalid - need to sort the order
-
-
-
-
-function convert_to_pence(double value){
-    int pounds = (int)value;
-    int pennies = (int)((value - pounds) * 100);
-    String result = String.Format("{0:#0} Lt {1:00} ct", pounds, pence);
+function validate_input(object) {
+    // regex - modified from http://regex101.com/r/fH3lV1/2 to fit sterling ( optional , as thousand separator . as decimal separator)
+    // - explination at http://regex101.com/r/hT3aD0/1
+    // ^\s*-?(\u00A3|\s*)((\d{1,3}(,(\d){3})*)|\d*)(\.\d{1,2})?(\s?|p)\s*$
+    // regex doesnt make £p invalid - need to sort the order
+    var regex = /^\s*-?(\u00A3|\s*)((\d{1,3}(,(\d){3})*)|\d*)(\.\d{1,2})?(\s?|p)\s*$/;
+    if (regex.test(object.val())) {
+        // match
+        return true;
+    } else {
+        // no match
+        return false;
+    }
 }
 
+function convert_to_pence(value) {
+    // TODO convert the input into an int
+    int pounds = (int)value;
+    int pennies = (int)((value - pounds) * 100);
+    String result = String.Format("{0:#0}{1:00}", pounds, pence);
+}
 
+function input_has_error(object) {
+    // set the input box to error state
+    object.parent().removeClass('has-success');
+    object.parent().addClass('has-error');
+}
 
+function input_has_success(object) {
+    // set the input box to success state
+    object.parent().removeClass('has-error');
+    object.parent().addClass('has-success');
+}
 
-
-
-
-
-
-
-
+function run_input_values(object) {
+    if (validate_input(object)) {
+        input_has_success(object);
+    } else {
+        input_has_error(object);
+    }
+}
 
 // simple validator - does a match against the regex and sets a class on the input
 
-$(function() {
+$(function () {
 // bind keypresses on the form input to trigger the validator
-    $('#amount').on("input", function() {
-
-        var regex = /^\s*-?(\u00A3|\s*)((\d{1,3}(,(\d){3})*)|\d*)(\.\d{1,2})?(\s?|p)\s*$/;
-
-        if(regex.test($(this).val())){
-            $(this).parent().removeClass('has-error');
-            $(this).parent().addClass('has-success');
-            // match
-            //greedy_coins($(this).val());
-            
-        } else {
-            // no match
-            $(this).parent().removeClass('has-success');
-            $(this).parent().addClass('has-error');
-            
-        }
+    $('#amount').on("input", function () {
+        run_input_values($('#amount'));
     });
 });
